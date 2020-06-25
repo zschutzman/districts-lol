b8d48566798b26c20275a8641d9b84fdc320d647
zschutzman <zach@schutzman.net>
2020-06-25 18:58:43 -0400

    Update test_district.js

--- test_district.js
+++ test_district.js
@@ -32,6 +32,7 @@ map.fitBounds(grp.getBounds());
 
 function reloadmap(new_dist){
   layer.clearLayers()
-  layer = (L.GeoJSON.AJAX("geojsons/" + new_dist + ".geojson"))
+  layer = new L.GeoJSON.AJAX("geojsons/" + new_dist + ".geojson")
+  console.log(layer)
   map.fitBounds(grp.getBounds());
 }
